d511415ecd32473aeddaf0cd65219e84089a9d39
Rana Akbas <akbasnar@gmail.com>
2026-02-04 17:32:58 +0300

    Add session-scoped driver fixture for Android tests in conftest.py

--- tests/android/conftest.py
+++ tests/android/conftest.py
@@ -3,6 +3,15 @@ from config import get_driver
 from pages.android.onboard_page import OnboardPage
 from pages.android.premium_page import PremiumPage
 
+APP_PACKAGE = "com.san.authenticator"
+
+
+@pytest.fixture(scope="session")
+def driver():
+    driver = get_driver(test_name="Android Tests")
+    yield driver
+    driver.quit()
+
 
 @pytest.fixture(scope="function")
 def app():
@@ -14,6 +23,4 @@ def app():
     onboard.complete_onboarding()
     premium.skip_if_visible()
 
-    yield driver
-
-    driver.quit()
+    return driver
